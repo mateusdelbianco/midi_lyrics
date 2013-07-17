80d5328b8a53152c1069246d34b948e713a8b758
Mateus Del Bianco <mateus@delbianco.net.br>
2013-07-16 23:18:34 -0300

    tests start2 in all test cases

--- spec/midi_lyrics_spec.rb
+++ spec/midi_lyrics_spec.rb
@@ -11,85 +11,85 @@ describe MidiLyrics do
   context "parsing" do
     it "parses one_note_one_syllable.mid correctly" do
       lyrics = MidiLyrics::Parser.new("spec/fixtures/one_note_one_syllable.mid").extract
-      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, duration: x.duration } }
+      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { :text => "Test ", :start => 0.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "\n", :start => QUARTER_NOTE_DURATION, :duration => 0.0 }
+        { text: "Test ", start: 0.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "\n", start: QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 }
       ])
     end
 
     it "parses two_notes_one_syllable.mid correctly" do
       lyrics = MidiLyrics::Parser.new("spec/fixtures/two_notes_one_syllable.mid").extract
-      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, duration: x.duration } }
+      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { text: "Test ", start: 0, duration: 0.5 + QUARTER_NOTE_DURATION },
-        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, duration: 0 },
-        { text: "\n", start: 0.5 + QUARTER_NOTE_DURATION, duration: 0 }
+        { text: "Test ", start: 0, start2: 0.0, duration: 0.5 + QUARTER_NOTE_DURATION },
+        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0 },
+        { text: "\n", start: 0.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0 }
       ])
     end
 
     it "parses two_notes_two_syllables.mid correctly" do
       lyrics = MidiLyrics::Parser.new("spec/fixtures/two_notes_two_syllables.mid").extract
-      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, duration: x.duration } }
+      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { text: "Test", start: 0, duration: QUARTER_NOTE_DURATION },
-        { text: "ing ", start: 0.5, duration: QUARTER_NOTE_DURATION },
-        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, duration: 0 },
-        { text: "\n", start: 0.5 + QUARTER_NOTE_DURATION, duration: 0 }
+        { text: "Test", start: 0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "ing ", start: 0.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0 },
+        { text: "\n", start: 0.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0 }
       ])
     end
 
     it "parses spaces_and_returns.mid correctly" do
       lyrics = MidiLyrics::Parser.new("spec/fixtures/spaces_and_returns.mid").extract
-      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, duration: x.duration } }
+      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { text: "Test", start: 0.5, duration: QUARTER_NOTE_DURATION },
-        { text: "ing ", start: 1, duration: QUARTER_NOTE_DURATION },
-        { text: "\r", start: 1 + QUARTER_NOTE_DURATION, duration: 0 },
-        { text: "One", start: 1.5, duration: HALF_NOTE_DURATION },
-        { text: "Two", start: 2.5, duration: HALF_NOTE_DURATION },
-        { text: "Three ", start: 3.5, duration: HALF_NOTE_DURATION },
-        { text: "\r", start: 3.5 + HALF_NOTE_DURATION, duration: 0 },
-        { text: "\n", start: 3.5 + HALF_NOTE_DURATION, duration: 0 }
+        { text: "Test", start: 0.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "ing ", start: 1, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 1 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0 },
+        { text: "One", start: 1.5, start2: 0.0, duration: HALF_NOTE_DURATION },
+        { text: "Two", start: 2.5, start2: 0.0, duration: HALF_NOTE_DURATION },
+        { text: "Three ", start: 3.5, start2: 0.0, duration: HALF_NOTE_DURATION },
+        { text: "\r", start: 3.5 + HALF_NOTE_DURATION, start2: 0.0, duration: 0 },
+        { text: "\n", start: 3.5 + HALF_NOTE_DURATION, start2: 0.0, duration: 0 }
       ])
     end
 
     it "parses repeating_lyrics.mid correctly repeating" do
       lyrics = MidiLyrics::Parser.new("spec/fixtures/repeating_lyrics.mid").extract
-      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, duration: x.duration } }
+      lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { :text => "Test", :start => 0.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "ing ", :start => 0.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 0.5 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "One", :start => 1.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Two", :start => 1.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Three ", :start => 2.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 2.0 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "\n", :start => 2.0 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "Test", :start => 2.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "ing ", :start => 3.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 3.0 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "One", :start => 3.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Two", :start => 4.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Three ", :start => 4.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 4.5 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "\n", :start => 4.5 + QUARTER_NOTE_DURATION, :duration => 0.0 },
+        { text: "Test", start: 0.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "ing ", start: 0.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "One", start: 1.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "Two", start: 1.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "Three ", start: 2.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 2.0 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "\n", start: 2.0 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "Test", start: 2.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "ing ", start: 3.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 3.0 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "One", start: 3.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "Two", start: 4.0, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "Three ", start: 4.5, start2: 0.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 4.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
+        { text: "\n", start: 4.5 + QUARTER_NOTE_DURATION, start2: 0.0, duration: 0.0 },
       ])
     end
 
     it "parses repeating_lyrics.mid correctly not repeating" do
-      lyrics = MidiLyrics::Parser.new("spec/fixtures/repeating_lyrics.mid", :repeating => false).extract
+      lyrics = MidiLyrics::Parser.new("spec/fixtures/repeating_lyrics.mid", repeating: false).extract
       lyrics = lyrics.collect{|x| { text: x.text, start: x.start, start2: x.start2, duration: x.duration } }
       expect(lyrics).to eq([
-        { :text => "Test", :start => 0.0, :start2 => 2.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "ing ", :start => 0.5, :start2 => 3.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 0.5 + QUARTER_NOTE_DURATION, :start2 => 3.0 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "One", :start => 1.0, :start2 => 3.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Two", :start => 1.5, :start2 => 4.0, :duration => QUARTER_NOTE_DURATION },
-        { :text => "Three ", :start => 2.0, :start2 => 4.5, :duration => QUARTER_NOTE_DURATION },
-        { :text => "\r", :start => 2.0 + QUARTER_NOTE_DURATION, :start2 => 4.5 + QUARTER_NOTE_DURATION, :duration => 0.0 },
-        { :text => "\n", :start => 2.0 + QUARTER_NOTE_DURATION, :start2 => 4.5 + QUARTER_NOTE_DURATION, :duration => 0.0 },
+        { text: "Test", start: 0.0, start2: 2.5, duration: QUARTER_NOTE_DURATION },
+        { text: "ing ", start: 0.5, start2: 3.0, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 0.5 + QUARTER_NOTE_DURATION, start2: 3.0 + QUARTER_NOTE_DURATION, duration: 0.0 },
+        { text: "One", start: 1.0, start2: 3.5, duration: QUARTER_NOTE_DURATION },
+        { text: "Two", start: 1.5, start2: 4.0, duration: QUARTER_NOTE_DURATION },
+        { text: "Three ", start: 2.0, start2: 4.5, duration: QUARTER_NOTE_DURATION },
+        { text: "\r", start: 2.0 + QUARTER_NOTE_DURATION, start2: 4.5 + QUARTER_NOTE_DURATION, duration: 0.0 },
+        { text: "\n", start: 2.0 + QUARTER_NOTE_DURATION, start2: 4.5 + QUARTER_NOTE_DURATION, duration: 0.0 },
       ])
     end
   end
